cd2e6253965ffb4c213e9c68e4459973c27ed63e
Eduardo Landa <eduardo.landa@outlook.com>
2019-09-21 12:23:10 -0700

    3

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+require("dotenv").config();
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
 const pubsub = new PubSub();
 
@@ -13,12 +15,14 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub })
 });
 
+const port = process.env.PORT || 5000;
+
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    console.log("MongoDB Connected");
+    return server.listen({ port: port });
   })
-  .then((res) => {
+  .then(res => {
     console.log(`Server running at ${res.url}`);
   });
